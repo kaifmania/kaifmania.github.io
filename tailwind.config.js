3b887c5ff6e619f9f4b232c8bec40fbb750d3ae2
kaifmania <tojikunatojikuna@gmail.com>
2026-04-23 20:34:45 +0400

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  plugins: [require('daisyui')],
-  daisyui: {
-    themes: ['dark', 'light', 'forest', 'ocean', 'sunset', 'pastel', 'midnight', 'coffee', 'sky', 'cherry']
-  }
-}
